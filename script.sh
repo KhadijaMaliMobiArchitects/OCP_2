a84ede8f0bd6093fbb0b3e9658a321c474855f06
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-28 15:51:14 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 sudo su <<HERE
+docker login ocpkhcontainerregistry.azurecr.io -u ocpkhcontainerregistry -p ZzGvPO99ZUnEdFGbyo2HL6QYAuryGNI6Od0flx309y+ACRBwFdPE
 docker stop ocpcontainer
 docker rm ocpcontainer
 docker pull ocpkhcontainerregistry.azurecr.io/khadijamalimobiarchitects/ocp_2:latest
