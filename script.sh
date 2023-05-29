a9b8e91bb60cc704175ce39843d711e633eb6486
KhadijaMaliMobiArchitects <128594978+KhadijaMaliMobiArchitects@users.noreply.github.com>
2023-05-29 11:49:43 +0100

    Update script.sh
    delete credentials

--- script.sh
+++ script.sh
@@ -1,5 +1,4 @@
 sudo su <<HERE
-docker login ocpkhcontainerregistry.azurecr.io -u ocpkhcontainerregistry -p ZzGvPO99ZUnEdFGbyo2HL6QYAuryGNI6Od0flx309y+ACRBwFdPE
 docker stop ocpcontainer
 docker rm ocpcontainer
 docker pull ocpkhcontainerregistry.azurecr.io/khadijamalimobiarchitects/ocp_2:latest
